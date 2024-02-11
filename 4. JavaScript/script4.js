3faae88d0e2bb6e6af1ecd06eaa0e3da1b145c92
Neharika Rout <neharikarout01@gmail.com>
2024-02-11 12:37:37 +0530

    ...

--- 4. JavaScript/script4.js
+++ 4. JavaScript/script4.js
@@ -5,3 +5,6 @@ let div = document.createElement("div")
 div.innerHTML = "I have been inserted <b>by Neharika<b> "
 div.setAttribute("class","created");
 document.querySelector(".container").append(div);
+
+let cont = document.querySelector(".container")
+cont.insertAdjacentHTML("beforebegin", "<b> I am under the water. Please h elp me here too much raining.... iuuuuooooo</b>")
