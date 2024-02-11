992369251d733f90ba7de300aec580629b5c52b7
Neharika Rout <neharikarout01@gmail.com>
2024-02-11 12:34:11 +0530

    ....

--- 4. JavaScript/script4.js
+++ 4. JavaScript/script4.js
@@ -1,2 +1,7 @@
 console.log(document.body)
 console.log("Neharika Rout")
+
+let div = document.createElement("div")
+div.innerHTML = "I have been inserted <b>by Neharika<b> "
+div.setAttribute("class","created");
+document.querySelector(".container").append(div);
