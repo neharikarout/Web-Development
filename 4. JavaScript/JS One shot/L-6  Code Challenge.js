179efd0a33c7d4c2442d3bb7736f686980a914ad
Neharika Rout <neharikarout01@gmail.com>
2024-02-18 14:29:44 +0530

    ...

---
+++ 4. JavaScript/JS One shot/L-6  Code Challenge.js
@@ -0,0 +1,9 @@
+// First Code Challenge 
+
+// Write code that will return a random letter from your name
+
+const Name = "Neharika";
+
+console.log(Name.charAt(Math.floor(Math.random() * Name.length)));
+
+
