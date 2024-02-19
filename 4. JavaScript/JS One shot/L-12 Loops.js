45b041fbd71a4753e1e0c29ed184e53244941368
Neharika Rout <neharikarout01@gmail.com>
2024-02-19 18:30:23 +0530

    ...

---
+++ 4. JavaScript/JS One shot/L-12 Loops.js
@@ -0,0 +1,49 @@
+// // Loops
+
+// let myNumber = 0;
+
+// // while loop
+
+// while(myNumber<50)
+// {
+//     console.log(myNumber);
+//     myNumber++;
+// }
+
+// // don't create an infinite loop
+
+// // do-while loop
+
+// do{
+//     console.log(myNumber);
+// }
+// while(myNumber<50){
+//     console.log(myNumber);
+//     myNumber+=2;
+
+// }
+
+// // For loops
+
+// for(let i=0; i<=10 ; i++)
+// {
+//     console.log(i);
+// }
+
+
+// let Name = "Neharika";
+// for(let i=0;i<Name.length;i++)
+// {
+//     console.log(Name.charAt(i));
+// }
+
+// let counter = 0;
+// let myLetter ;
+// let Name = "Neharika";
+// while(true)
+// {
+//     myLetter = Name[counter];
+//     console.log(myLetter);
+//     if(myLetter === "k") break;
+//     counter++;
+// }
