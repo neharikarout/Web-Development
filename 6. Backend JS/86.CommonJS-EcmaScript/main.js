051d76760dacb19ef10e22aee1aae9d27ced9566
Neharika Rout <neharikarout01@gmail.com>
2024-03-03 21:20:01 +0530

    ...

--- 6. Backend JS/86.CommonJS-EcmaScript/main.js
+++ 6. Backend JS/86.CommonJS-EcmaScript/main.js
@@ -1,14 +1,16 @@
-const http = require('node:http');
+// import {a, b, d} from "./mymodule.js"
+// console.log(a, b, d)
 
-const hostname = '127.0.0.1';
-const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1> Hello world </h1>');
-});
+// import harry from "./mymodule.js"
+// console.log(harry)
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// (function(exports, require, module, __filename, __dirname) {
+
+//     // Module code actually lives here
+  
+//   });
+
+const a = require("./mymodule2.js")
+
+console.log(a, __dirname, __filename)
