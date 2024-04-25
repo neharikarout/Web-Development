8465cace12954759351d4aed8223bc7fa733bd87
Neharika Rout <neharikarout01@gmail.com>
2024-04-25 17:16:36 +0530

    ...

---
+++ 4. JavaScript/JS/1.variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "neharika@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId = 2 // not allowed
+
+accountEmail = "nrnr@gmail.com"
+accountPassword = "789456"
+accountCity = "Delhi"
+
+console.log(accountId);
+console.log([accountId , accountEmail , accountPassword , accountCity])
+
+/* 
+Prefer not to use var
+because of issur in block scope and functional scope
+*/
