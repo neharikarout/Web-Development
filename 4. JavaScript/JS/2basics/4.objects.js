343f0f6aa0a40a5394cf937d98bc6296d59f18ea
Neharika Rout <neharikarout01@gmail.com>
2024-05-19 06:44:31 +0530

    json and object destructuring

--- 4. JavaScript/JS/2basics/4.objects.js
+++ 4. JavaScript/JS/2basics/4.objects.js
@@ -58,13 +58,67 @@ const users = [
 
 ]
 
-console.log(users[1].email) // accessing array of objects : nr@gmail.com
+// console.log(users[1].email) // accessing array of objects : nr@gmail.com
 
 
-console.log(redditUser)
+// console.log(redditUser)
 
-console.log(Object.keys(redditUser)) // very imp to know the keys : [ 'id', 'name', 'isLoggedIn' ]
-console.log(Object.values(redditUser)) // [ '123abc', 'Ian', false ]
-console.log(Object.entries(redditUser)) //[ [ 'id', '123abc' ], [ 'name', 'Ian' ], [ 'isLoggedIn', false ] ]
+// console.log(Object.keys(redditUser)) // very imp to know the keys : [ 'id', 'name', 'isLoggedIn' ]
+// console.log(Object.values(redditUser)) // [ '123abc', 'Ian', false ]
+// console.log(Object.entries(redditUser)) //[ [ 'id', '123abc' ], [ 'name', 'Ian' ], [ 'isLoggedIn', false ] ]
 
-console.log(redditUser.hasOwnProperty('isLoggedIn')) // to check whether this property exists in the object
+// console.log(redditUser.hasOwnProperty('isLoggedIn')) // to check whether this property exists in the object
+
+
+
+
+
+
+//              Destructuring of objects
+const course = {
+    coursename: "js",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+// const {courseInstructor} = course
+
+// can name on your own 
+
+const {courseInstructor : instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+
+// const navbar = (props.company) => {
+
+// }
+
+// navbar(company= "hitesh")
+
+// instead of this we do this in react
+
+// const navbar = ({company}) => { // destructuring
+  
+// }
+// navbar(company = "hitesh")
+
+
+// object in Api comes in form of JSON format
+
+// {
+//     "name": "Neharika",
+//     "courseName" : "js in hindi",
+//    " price": "free"
+// }
+
+// one api url : https://api.github.com/users/neharikarout
+
+// sometimes api comes in array format ; and array has many objects .
+// so first destructure array then object
+
+// tools json formatter
