aff9507aed5d599fdf04be3f4b8006d1e1c3bb63
Neharika Rout <neharikarout01@gmail.com>
2024-05-22 22:46:04 +0530

    Dom manipulation

--- 4. JavaScript/JS/6.DOM/one.js
+++ 4. JavaScript/JS/6.DOM/one.js
@@ -34,3 +34,32 @@ document.querySelector('input[type="password"]')
 const myul = document.querySelector('ul')
 const item = myul.querySelector('li')
 item.style.backgroundColor="green"
+
+
+
+// node list has not map() function
+tempList = document.querySelectorAll("li") // returns all elements in node list
+// it is somewhat different from array but has some functionality like array
+tempList.style.color="green" // not applicable
+tempList[0].style.color="green" // now applicable to first element
+
+
+tempList.foreach(function(l) {
+    l.style.backgroundColor = 'green'
+}) // to apply to all elements at once
+
+
+
+const tempClassList = document.getElementsByClassName('list-item') // return all elements in html collection
+
+// it has no method to apply loop
+
+
+// so we have to convert it to array
+Array.from(tempClassList)
+
+const myConvertedArray = Array.from(tempClassList)
+
+myConvertedArray.forEach(function(li){
+    li.style.color= "orange"
+})
