69e9d55c734b7c442ddd0793c54002842e70535a
Neharika Rout <neharikarout01@gmail.com>
2024-05-28 22:59:42 +0530

    created first test backend app

--- 6. Backend JS/backend/index.js
+++ 6. Backend JS/backend/index.js
@@ -1,11 +1,27 @@
+require('dotenv').config()
 const express = require("express");
-const app = express();
+// import express from "express"
+
+const app = express(); // very powerful variable
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
+app.get("/twitter",(req,res) => {
+    res.send('neharikaroutdotcom')
+})
+
+app.get('/login',(req,res) => {
+  res.send('<h1> Server Created Successfully</h1>')
+})
+
+app.get('/youtube',(req,res) => {
+  res.send('<h2> youtube app </h2>')
+})
+
+// using .env variable
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`);
 });
