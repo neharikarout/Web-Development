f396fb434c80b9aeb581ec365f41c580530fd8d9
Neharika Rout <neharikarout01@gmail.com>
2024-05-28 21:53:29 +0530

    delete files

--- 6. Backend JS/85.BackendNodeNpm.js/server.js
+++
@@ -1,8 +0,0 @@
-var slugify = require('slugify')
-
-let a = slugify('some string')
-console.log(a)
-
-// if you prefer something other than '-' as separator
-const b = slugify('some st&&#%@%^ring') // some string
-console.log(b)
