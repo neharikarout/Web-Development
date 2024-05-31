adbfe3a385b2ac2dc430051c0bb50752843e2be8
Neharika Rout <neharikarout01@gmail.com>
2024-05-31 06:42:01 +0530

    updated index.js

--- 6. Backend JS/2server/index.js
+++ 6. Backend JS/2server/index.js
@@ -2,46 +2,49 @@ const http = require('http')
 const fs = require('fs')
 const url = require('url')
 
+function myHandler (req, res){
+ // we can use that req.url
+ if(req.url === "/favicon.ico") return res.end();
+ const log = `${Date.now()}: ${req.method} ${req.url} New Request Received \n`
+ const myUrl = url.parse(req.url,true) // it will parse the url into string tokens
+
+ 
+ fs.appendFile('log.txt',log,(err,data) => {
+     switch(myUrl.pathname){
+         case '/':
+             if(req.method === "GET") res.end("HOMEPAGE");
+         break;
+         case '/about': 
+         const username = myUrl.query.myname;
+         res.end(` Hi , ${username}`);
+         break;
+         case "/signup":
+             if(req.method === "GET") res.end('This is a signup form');
+             else if(req.method === "POST"){
+                 // DB Query
+                 res.end("Success")
+             }
+             break;
+         default:
+             res.end("404 Not Found")
+
+     }
+ })
+}
+
 // the callback here is request listener. and is responsible for handling requests. req,res are request handlers
-const myServer = http.createServer((req,res) => {
+const myServer = http.createServer(myHandler)
     // creating log
     // const log = `${Date.now()}: ${req.url} New Request Received \n`
     // fs.appendFile('log.txt',log,(err,data) => {
     //     res.end('hello ! from Server')
     // })
 
-    // we can use that req.url
-    if(req.url === "/favicon.ico") return res.end();
-    const log = `${Date.now()}: ${req.method} ${req.url} New Request Received \n`
-    const myUrl = url.parse(req.url,true) // it will parse the url into string tokens
-
-    fs.appendFile('log.txt',log,(err,data) => {
-        switch(myUrl.pathname){
-            case '/':
-                if(req.method === "GET") res.end("HOMEPAGE");
-            break;
-            case '/about': 
-            const username = myUrl.query.myname;
-            res.end(` Hi , ${username}`);
-            break;
-            case "/signup":
-                if(req.method === "GET") res.end('This is a signup form');
-                else if(req.method === "POST"){
-                    // DB Query
-                    res.end("Success")
-                }
-                break;
-            default:
-                res.end("404 Not Found")
-
-        }
-    })
 
 
     //console.log(req)
     //console.log("New request received");
     // res.end('hello ! from Server')
-});
 
 // to run a server we need a port to listen.
 // one port can have one server
