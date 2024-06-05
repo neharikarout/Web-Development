52e4cb5fb136b5ae4f2621bbbbd695ecdd78388c
Neharika Rout <neharikarout01@gmail.com>
2024-06-05 10:48:16 +0530

    refactor my first backend

--- 6. Backend JS/5refactor/index.js
+++ 6. Backend JS/5refactor/index.js
@@ -12,12 +12,13 @@ const PORT =  8000;
 
 // connection
 connectMongoDb("mongodb://127.0.0.1:27017/app-1")
+.then(() => console.log("mongodb connected"))
 
 // Middlewares
 app.use(express.urlencoded({extended : false}))
 app.use(logReqRes("log.txt"))
 
 // Routes
-app.use = ("/user", userRouter)
+app.use("/api/users", userRouter)
 
 app.listen(PORT, () => console.log(`Server started at Port: ${PORT}`))
