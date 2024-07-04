9ea09c6ddd04fb28f6b97d33db2192e4f8785476
Neharika Rout <neharikarout01@gmail.com>
2024-07-04 19:34:34 +0530

    functions

---
+++ 6. Backend JS/backend-domination/script.js
@@ -0,0 +1,12 @@
+// fundamentals of js:
+// arrays and objects
+// functions return 
+// async js coding
+
+var arr = [1,2,3,4];
+
+// foreach map filter find indexof
+
+arr.forEach(function(value){
+    console.log(value + "hello");
+})
---
+++ Frontend Practice/JS/script.js
@@ -0,0 +1,26 @@
+// functions - js mein function normal programming jaise nahi hai, iska matlab hai , yaha par js mein, functions banaane k liye type nahi batana pdta.
+// bus have to use Keyword "function" and can create using any name
+
+// indepth - js mein functions are first class functions i.e js mein functions ko variable/value treat kiya ja skta h
+
+
+// what is function
+// - koi esa code jisko baar baar use kar ske , ya fir turant na chalana ho
+
+function abcd() {
+    console.log("hello");
+}
+
+abcd() // function calling
+
+// why we need
+// to reuse code and  to wrap the code which we want to run in future at some point of time
+
+
+// how to use
+
+
+// extras we should know
+// parameters and arguements
+
+
