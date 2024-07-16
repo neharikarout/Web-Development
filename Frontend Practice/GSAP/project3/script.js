744a9dea54814e440b2f40f230bad2022b263037
Neharika Rout <neharikarout01@gmail.com>
2024-07-16 11:27:03 +0530

    gsap

---
+++ Frontend Practice/GSAP/project3/script.js
@@ -0,0 +1,34 @@
+// gsap.from("#page1 #circle",{
+//     scale: 0,
+//     delay: 1,
+//     duration: 2,
+//     rotate: 720,
+// })
+
+// gsap.from("#page2 #circle",{
+//     scale: 0,
+//     duration: 2,
+//     rotate: 720,
+//     scrollTrigger: {
+//         trigger: "#page2 #circle",
+//         scroller: "body",
+//         markers: "true",
+//         start: "top 60%",
+//         end: "top 30%",
+//         scrub: 2
+//         }
+// })\
+
+
+gsap.to("#page2 img",{
+    width: "100%",
+    scrollTrigger: {
+        trigger: "#page2",
+        scroller: "body",
+        markers: true,
+        start: "top 0",
+        end: "top -100%",
+        scrub: 2,
+        pin: true,
+    }
+})
