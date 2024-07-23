f97c729751a18bc7e1f2b8e59c67c349f78e30fb
Neharika Rout <neharikarout01@gmail.com>
2024-07-23 18:36:47 +0530

    useRef

--- Frontend Practice/React/project1/src/App.jsx
+++ Frontend Practice/React/project1/src/App.jsx
@@ -1,45 +1,26 @@
-import React, { useState } from 'react';
-import Navbar from './components/Navbar';
-import Songs from './components/Songs';
+import React, { useRef, useState } from 'react';
+
 
 
 function App() {
 
-  const data= [
-    {img:"https://images.unsplash.com/photo-1518640467707-6811f4a6ab73?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGFic3RyYWN0fGVufDB8fDB8fHww", name:"Take Two", artist:"Bangtan", added: false},
-    {img:"https://images.unsplash.com/photo-1518640467707-6811f4a6ab73?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGFic3RyYWN0fGVufDB8fDB8fHww", name:"Closer Than This", artist:"JM", added: true},
-    {img:"https://images.unsplash.com/photo-1518640467707-6811f4a6ab73?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGFic3RyYWN0fGVufDB8fDB8fHww", name:"Rockstar", artist:"Lisa", added: false},
-    {img:"https://images.unsplash.com/photo-1518640467707-6811f4a6ab73?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGFic3RyYWN0fGVufDB8fDB8fHww", name:"Spot", artist:"Zico & Jennie", added: false},
-    {img:"https://images.unsplash.com/photo-1518640467707-6811f4a6ab73?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGFic3RyYWN0fGVufDB8fDB8fHww", name:"Super", artist:"Seventeen", added: false},
-    {img:"https://images.unsplash.com/photo-1518640467707-6811f4a6ab73?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGFic3RyYWN0fGVufDB8fDB8fHww", name:"Small Girl", artist:"LYJ", added: true},
-    {img:"https://images.unsplash.com/photo-1518640467707-6811f4a6ab73?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGFic3RyYWN0fGVufDB8fDB8fHww", name:"Lost", artist:"RM", added: false},
-  ]
 
-  const[songData , setSongData] = useState(data);
-  const handleClick = (index)=> {
-    setSongData((prev)=>{
-      return prev.map((item,itemIndex)=>{
-        if(itemIndex === index) return{...item,added: !item.added}
-        return item;
+  const name = useRef(null);
+  const age = useRef(null);
 
-      })
-    })
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(name.current.value, age.current.value)
   }
-
-  
   
 
   return (
     <>
-      <div className='w-full h-full bg-zinc-300  p-4 '>
-        <Navbar data={songData}/>
-         <div className='px-20 flex gap-10 mt-10 flex-wrap'>
-         {songData.map((obj,index)=>{
-          return <Songs key={index} data={obj} handleClick={handleClick}  index={index}/>
-         })}
-         </div>
-        
-      </div>
+     <form action='' onSubmit={handleSubmit}>
+      <input ref={name} placeholder='name' type='text'/>
+      <input ref={age} placeholder='age' type='text'/>
+      <input type='submit' />
+     </form>
     </> 
   )
 }
